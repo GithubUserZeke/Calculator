09a4ab09f37ab052d50dddb67eaa5ee920ea1521
Ezequiel <isimpledistance@gmail.com>
2025-01-23 14:19:44 -0500

    image of glovali added

--- Program.py
+++ Program.py
@@ -1,1 +1,3 @@
-print("Hello World")
+print("A picture of Glovali")
+
+![Glovali!](https://i.ytimg.com/vi/juD7iJ76ODo/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDmSzG8Qy4aOiSggghpmhlZVl9mpA "Solar Glovali") 
